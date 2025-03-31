7f673019ef014a1617cd2b5f0ac53995c889a575
mihas35 <voroninm898@gmail.com>
2025-04-01 02:57:16 +1000

    аоом

--- plugins/gc-revoke.js
+++ plugins/gc-revoke.js
@@ -2,7 +2,7 @@
 
 let handler = async(m, { conn }) => {
 let revoke = await conn.groupRevokeInvite(m.chat)
-await conn.reply(m.chat, `🔹️ *_Связь группы была успешно восстановлена._*\n♾ • Новая ссылка: ${'https://chat.whatsapp.com/' + revoke}`, m)}
+await conn.reply(m.chat, `🔹️ *_Связь группы была успешно восстановлена._*\n ♾ • *Новая ссылка:* ${'https://chat.whatsapp.com/' + revoke}`, m)}
 handler.command = ['сбросить_ссылку', 'revoke']
 handler.botAdmin = true
 handler.admin = true
