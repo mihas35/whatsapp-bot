df21d61e3b845c43e162d97bb26616a6b53d50b5
mihas35 <voroninm898@gmail.com>
2025-04-01 04:21:32 +1000

    нопнш

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -287,11 +287,6 @@ let vn = './media/я приду к тебе.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
-if (!chat.isBanned && chat.audios && m.text.match(/(я тут)/gi)) {    
-let vn = './media/ятут.mp3'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
-
 if (!chat.isBanned && chat.audios && m.text.match(/(лс|личку)/gi)) {    
 let vn = './media/лс.mp4'
 this.sendPresenceUpdate('recording', m.chat)   
